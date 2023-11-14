51f4712fa8894c5c531b765a51647c8ed7de8e95
TaraRhoseyn <taragulwell@gmail.com>
2023-11-14 10:35:35 +0000

    Add principles preview section to home page

--- app/app.js
+++ app/app.js
@@ -21,23 +21,24 @@ const pages = [
     new Page('glossary', 'Glossary'),
 ]
 
+
 const blogs = [
     // Blog(filename, title, author, authorJob, date, profilePicture)
     new Blog(
         'blog1', 'Lost at the National Centre', 'Paul Batcup', 
-        util.profile.paul_batcup_job, 'October 15, 2023', util.profile.paul_batcup_profile),
+        util.job.paul_batcup_job, 'October 15, 2023', util.profile.paul_batcup_profile),
     new Blog(
         'blog2', 'How we created the new Digital and Service Design Principles', 'Tara Rhoseyn', 
-        util.profile.tara_rhoseyn_job, 'Oct 11, 2023', util.profile.tara_rhoseyn_profile),
+        util.job.tara_rhoseyn_job, 'Oct 11, 2023', util.profile.tara_rhoseyn_profile),
     new Blog(
         'blog3', 'Why we need a Digital and Service Design Team', 'Phil Stephens', 
-        util.profile.phil_stephens_job, 'October 1, 2023', util.profile.phil_stephens_profile),
+        util.job.phil_stephens_job, 'October 1, 2023', util.profile.phil_stephens_profile),
     new Blog(
         'blog4', 'National Centre - The Digital Journey Begins', 'Craig Nowell', 'Assistant Director National Sport Facilities, Infrastructure and Capital', 'October 15, 2023', 
         util.profile.generic_user_profile),
     new Blog(
         'blog5', 'Take a giant leap... With one small step', 'Paul Batcup', 
-        util.profile.paul_batcup_job, 'October 15, 2023', util.profile.paul_batcup_profile),
+        util.job.paul_batcup_job, 'October 15, 2023', util.profile.paul_batcup_profile),
 ]
 
 function createPages(type) {
