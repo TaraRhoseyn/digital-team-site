d71dbe9b6db01c5678df371bcefa11470bb70a43
TaraRhoseyn <taragulwell@gmail.com>
2024-01-09 16:34:45 +0000

    add external link to favicon

--- app/app.js
+++ app/app.js
@@ -28,14 +28,14 @@ const pages = [
 const blogs = [
     // Blog(filename, title, author, authorJob, date, profilePicture)
     new Blog(
-        'blog1', 'Lost at the National Centre', 'Paul Batcup', 
-        util.job.paul_batcup_job, '15th October, 2023', util.profile.paul_batcup_profile),
+        'blog1', 'Why we need a Digital and Service Design Team', 'Phil Stephens', 
+        util.job.phil_stephens_job, '1st October, 2023', util.profile.phil_stephens_profile),
     new Blog(
         'blog2', 'How we created the new Digital and Service Design Principles', 'Tara Rhoseyn', 
         util.job.tara_rhoseyn_job, '11th October, 2023', util.profile.tara_rhoseyn_profile),
     new Blog(
-        'blog3', 'Why we need a Digital and Service Design Team', 'Phil Stephens', 
-        util.job.phil_stephens_job, '1st October, 2023', util.profile.phil_stephens_profile),
+        'blog3', 'Lost at the National Centre', 'Paul Batcup', 
+        util.job.paul_batcup_job, '15th October, 2023', util.profile.paul_batcup_profile),
     new Blog(
         'blog4', 'National Centre - The Digital Journey Begins', 'Craig Nowell', 'Assistant Director National Sport Facilities, Infrastructure and Capital', '15th October, 2023', 
         util.profile.generic_user_profile),
@@ -45,6 +45,9 @@ const blogs = [
     new Blog(
         'blog6', 'Meet the Newbies', 'Paul Batcup', 
         util.job.paul_batcup_job, '16th December, 2023', util.profile.paul_batcup_profile),
+    new Blog(
+        'blog7', 'Defining the User Journey Process', 'Steffan Berrow', 
+        util.job.steffan_berrow_job, '9th December, 2023', util.profile.steffan_berrow_profile),
 ]
 
 function createPages(type) {
